8198025457afdac2fbd5f9786cfe7a870641deaa
Basil_Stump <bastumpo@gmail.com>
2024-02-11 14:28:21 +0800

    1480 solved

--- main.js
+++ main.js
@@ -3,5 +3,3 @@ import { createCounter } from "./problems/2665.js";
 import { map } from "./problems/2635.js";
 import { runningSum } from "./problems/1480.js";
 
-console.log(runningSum[1,2,3,4])
-
--- problems/1480.js
+++ problems/1480.js
@@ -1,5 +1,34 @@
+// export const runningSum = (nums) => {
+//   let currentSum = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     currentSum += nums[i];
+//     nums[i] = currentSum;
+//   }
+
+//   return nums;
+// };
+
+// export const runningSum = (nums) => {
+//   const result = [];
+
+//   nums.reduce((acc, num) => {
+//     result.push((acc += num));
+
+//     return acc;
+//   }, 0);
+
+//   return result;
+// };
+
+// export const runningSum = (nums) => {
+//   return nums.reduce((acc, n, i) => {
+//     if (!acc.length) return [n];
+//     else return [...acc, (n += acc[i - 1])];
+//   }, []);
+// };
+
 export const runningSum = (nums) => {
-  return nums.reduce((result, num) => {
-    return num + result;
-  }, 0);
+  nums.reduce((acc, n, i, arr) => (arr[i] += acc));
+  return nums;
 };
