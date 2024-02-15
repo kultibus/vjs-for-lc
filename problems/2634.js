f8b21ff692a7e6f94b3b8c62bf1674a1b4cc28aa
Basil_Stump <bastumpo@gmail.com>
2024-02-15 15:13:45 +0800

    2634

---
+++ problems/2634.js
@@ -0,0 +1,9 @@
+export const filter = (arr, fn) => {
+  const filteredArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i]) || fn(arr[i], i)) filteredArr.push(arr[i]);
+  }
+
+  return filteredArr;
+};
